d71755d561b74b9463ac4745d14af24fa6afa6c8
ElizabethBrooks <merimaat1@gmail.com>
2015-12-01 01:59:37 -0800

    Web-app and EF database GUI using MVC
    Entity framework database is connected to a web-application interface
    using ASP.NET, ADO.NET, and Model View Controller (MVC).

---
+++ DatabaseProject_UESL/DatabaseProject_UESL/Course.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Course]
+(
+	[ClassRecordID] INT IDENTITY (1, 1) NOT NULL,
+    [CatalogNumber] NVARCHAR (50) NULL,
+    [ClassName] NVARCHAR (50) NULL,
+    [SectionNumber] INT NULL,
+	[Instructor] NVARCHAR (50) NULL,
+	[MeetTime] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([ClassRecordID] ASC)
+)
---
+++ DatabaseProject_UESL/DatabaseProject_UESL/Student.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[UniqueRecordID] INT IDENTITY (1, 1) NOT NULL,
+	[StudentID] INT Null,
+    [FirstName] NVARCHAR (50) NULL,
+    [LastName] NVARCHAR (50) NULL,
+    [LetterGrade] NVARCHAR (50) NULL,
+	[Placement] NVARCHAR (50) NULL,
+	[QuarterOfPlacement] NVARCHAR (50) NULL,
+	[CourseCatalogNumber] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([UniqueRecordID] ASC)
+)
